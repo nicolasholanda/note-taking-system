151e99785c5c2735e20a21fc2c244e7619eb1d7f
nicolasholanda <nikolas.nhm14@gmail.com>
2026-04-30 08:13:36 -0300

    Initial commit

---
+++ src/main/java/com/notetaking/NoteTakingApplication.java
@@ -0,0 +1,12 @@
+package com.notetaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteTakingApplication.class, args);
+    }
+}
